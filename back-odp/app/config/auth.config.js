82c064a6b0ca5b71fc044e509fc4c8d896f24a8c
amhanisa <azka@amhanisa.com>
2021-12-17 15:21:47 +0700

    hide secret key

--- back-odp/app/config/auth.config.js
+++ back-odp/app/config/auth.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  secret: "amhanisa-secrect-key",
-  // jwtExpiration: 3600,
-  // jwtRefreshExpiration: 86400,
+  secret: process.env.JWT_SECRET_KEY,
+  jwtExpiration: 3600,
+  jwtRefreshExpiration: 86400,
 
-  jwtExpiration: 1000,
-  jwtRefreshExpiration: 3600,
+  // jwtExpiration: 1000,
+  // jwtRefreshExpiration: 3600,
 };
